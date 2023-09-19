f1cf86bac25ce0a5accd9d5b6185e7e8ff3f4b6c
instinctgamingv2 <145374968+instinctgamingv2@users.noreply.github.com>
2023-09-19 15:22:33 +0100

    XD

---
+++ resources/[cfx-default]/[gameplay]/cdev_chat/public/config/config.lua
@@ -0,0 +1,61 @@
+PublicSharedChatConfig = {
+    -- 💬 Chat related configuration
+    Chat = {
+        -- 👀 How many messages can be visible in the chat history
+        MessageHistoryLimit = 25,
+
+        -- 📜 How many characters can be in a message (will be truncated if longer than this)
+        MessageSizeLimit = 5000,
+
+        -- 📷 Allow the player to pan the camera while the chatbox is open
+        AllowPanCameraWhileChatboxOpen = true,
+
+        -- ❌ Close the chatbox when the player sends a message
+        CloseChatboxOnSend = false,
+    },
+
+    -- 📜 Chat Manager Menu related configuration
+    ChatManager = {
+        -- 👮 Permission level (see https://docs.cdev.shop/fivem/cdev-library under your framework page)
+        PermissionLevel = PERMISSION_HIGH,
+
+        -- 👮 ACE permission
+        ACEPermission = "cdev_chat.manager",
+
+        -- ⌨️ Command to open the menu
+        Command = "chat",
+    },
+
+    -- 🎨 Chat theme colors, sizes, etc
+    Theme = {
+        PopUpBackgroundColor = "#EEC840",
+        PopUpBackgroundShadow = "#EEC840",
+        PopUpIconMentionBackgroundColor = "#FF2525",
+        PopUpIconNewMessageBackgroundColor = "#EEC840",
+        ChatboxBackgroundColor = "rgb(0 0 0 / 10%)",
+        MessageIDBackgroundColor = "#EEC840",
+        MessageIDTextColor = "#000",
+        MessageTextColor = "#ffffff",
+        MessageTextShadow = "0 0 2px black",
+        InputBoxBackgroundColor = "rgba(0, 0, 0, 0.65)",
+        InputBoxBorderColor = "rgba(255, 255, 255, 0.15)",
+        MainContentChatWidth = "63rem",
+        MessageBodyChatHeight = "23rem",
+        IconTypeMessageSize = "3rem",
+        IconIdMessageSize = "3rem",
+        FontIdSizeMessage = "1.4rem",
+        FontSizeMessage = "1.6rem",
+        InputSizeMessage = "1.7rem 2rem",
+        IconSendInputSize = "1.5rem",
+        IconChannelSize = "3.5rem",
+        MentionPeekTime = 2.5,
+        MessagePeekTime = 1.5,
+    },
+    -- By default icons are automatically loaded from "data/icons/" folder, however if that's not working it will grab them from here
+    FallbackIcons = {
+        "alert.svg",
+        "megaphone.svg",
+        "message.svg",
+        "person.svg",
+    }
+}
---
+++ resources/[cfx-default]/[gameplay]/cdev_chat/public/server/api.lua
@@ -0,0 +1,107 @@
+ChatResourceAPI = {}
+
+-- 📛 Return the display ID for messages from a source
+ChatResourceAPI.GetMessageID = function(source)
+    return source
+end
+
+-- 🛂 Return the display name for messages from a source
+ChatResourceAPI.GetMessageName = function(source)
+    return clib.api.Character.GetCharacterNameFromSource(source)
+end
+
+-- 📨 Handle a message before it is sent to clients
+---@param channel string The channel name
+---@param message string The message
+---@param mentions number[] The mentioned player IDs
+---@return boolean . Whether the message should be sent to clients
+ChatResourceAPI.HandleMessage = function(channel, message, mentions)
+    return true
+end
+
+-- ❓ Return the initial suggestions for the chat
+ChatResourceAPI.GetInitialSuggestions = function(source)
+    if clib.config.isQBCore then
+        clib.frameworks.QBCore.Commands.Refresh(source)
+        return {}
+    elseif clib.config.isESX then
+        -- ESX.RegisteredCommands is inaccesible
+        -- local _, mapped = table.mapNoKeys(clib.frameworks.ESX.RegisteredCommands, function(_, cmd)
+        --     return cmd.suggestion
+        -- end)
+
+        -- return mapped
+        return {}
+    elseif clib.config.isCustom then
+       -- 🔧 If using custom, implement your own code here
+        return {}
+    end
+end
+
+-- 📦 Database queries
+ChatResourceAPI.Queries = {
+    CheckIfChannelsTableExists = function()
+        return #clib.db.Query("SHOW TABLES LIKE 'cdev_chat_channels'") > 0
+    end,
+    CreateChannelsTable = function()
+        clib.db.Query([[
+            CREATE TABLE IF NOT EXISTS `cdev_chat_channels` (
+                `id` int(11) NOT NULL AUTO_INCREMENT,
+                `name` varchar(50) NOT NULL DEFAULT '0',
+                `icon` varchar(50) NOT NULL DEFAULT '0',
+                `bgColor` varchar(50) NOT NULL DEFAULT '0',
+                `allowMention` tinyint(4) NOT NULL DEFAULT 0,
+                `restrictions` longtext NOT NULL,
+                `range` int(11) NOT NULL DEFAULT 0,
+                `enabled` tinyint(4) NOT NULL DEFAULT 0,
+                PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+        ]])
+    end,
+    CreateDefaultChannels = function()
+        local framework_god_permission = clib.config.isQBCore and "god" or "superadmin"
+        local query = ([[
+            INSERT INTO `cdev_chat_channels` (`id`, `name`, `icon`, `bgColor`, `allowMention`, `restrictions`, `range`, `enabled`) VALUES
+            (1, 'OOC', 'message', '#EEC840', 1, '{"aces":{"write":[],"read":[]},"jobs":{"write":[],"read":[]},"permissions":{"write":[],"read":[]}}', -1, 1),
+            (2, 'IC', 'person', '#40DEEE', 1, '{"jobs":{"read":[],"write":[]},"aces":{"read":[],"write":[]},"permissions":{"read":[],"write":[]}}', 100, 1),
+            (3, 'Report', 'alert', '#EE40A4', 1, '{"permissions":{"read":["framework_god_permission"],"write":[]},"aces":{"read":[],"write":[]},"jobs":{"read":[],"write":[]}}', -1, 1),
+            (4, 'Broadcast', 'megaphone', '#EE4040', 1, '{"permissions":{"write":["framework_god_permission"],"read":[]},"aces":{"write":[],"read":[]},"jobs":{"write":[],"read":[]}}', -1, 1);
+        ]]):gsub("framework_god_permission", framework_god_permission)
+
+        clib.db.Query(query)
+    end,
+    FetchChannels = function()
+        return clib.db.Query("SELECT * FROM cdev_chat_channels")
+    end,
+    ---@param channel Channel
+    CreateChannel = function(channel)
+        return clib.db.Query("INSERT INTO cdev_chat_channels (name, icon, bgColor, allowMention, restrictions, `range`, enabled) VALUES (?, ?, ?, ?, ?, ?, ?)", {
+            channel.name,
+            channel.icon,
+            channel.bgColor,
+            channel.allowMention and 1 or 0,
+            json.encode(channel.restrictions),
+            channel.range,
+            channel.enabled and 1 or 0,
+        })
+    end,
+    ---@param channel Channel
+    UpdateChannel = function(channel)
+        return clib.db.Query("UPDATE cdev_chat_channels SET name = ?, icon = ?, bgColor = ?, allowMention = ?, restrictions = ?, `range` = ?, enabled = ? WHERE id = ?", {
+            channel.name,
+            channel.icon,
+            channel.bgColor,
+            channel.allowMention and 1 or 0,
+            json.encode(channel.restrictions),
+            channel.range,
+            channel.enabled and 1 or 0,
+            channel.id,
+        })
+    end,
+    ---@param channelId number
+    DeleteChannel = function(channelId)
+        return clib.db.Query("DELETE FROM cdev_chat_channels WHERE id = ?", {
+            channelId,
+        })
+    end,
+}
