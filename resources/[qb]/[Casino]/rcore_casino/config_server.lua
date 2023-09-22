42b0286c0a4c9910e3780820573ef6690e8a3493
QuirkyCarlos <69331291+QuirkyCarlos@users.noreply.github.com>
2023-09-22 20:54:33 +0100

    Added back Casino log

--- resources/[qb]/[Casino]/rcore_casino/config_server.lua
+++ resources/[qb]/[Casino]/rcore_casino/config_server.lua
@@ -1,5 +1,5 @@
 ServerConfig = {
-    Discord = false, -- whether to enable Discord notifications (check config_server.lua)
+    Discord = true, -- whether to enable Discord notifications (check config_server.lua)
     DiscordInterval = 60000 * 15, -- 15 mins
     HookUrl = "https://discord.com/api/webhooks/1138291753556906006/6DeKNI83I-b0bYd_zHcQbCOXYjAHLOhENUQIV6SNWML-TNU2FkpcHi1HylagXSOtyQXp",
 }
