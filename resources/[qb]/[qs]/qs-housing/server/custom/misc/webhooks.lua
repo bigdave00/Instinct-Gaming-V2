9cc981d440e1fec4d7e0c1dfbf9b7a629c801fb5
QuirkyCarlos <69331291+QuirkyCarlos@users.noreply.github.com>
2023-09-27 20:42:17 +0100

    Update webhooks.lua

--- resources/[qb]/[qs]/qs-housing/server/custom/misc/webhooks.lua
+++ resources/[qb]/[qs]/qs-housing/server/custom/misc/webhooks.lua
@@ -1,10 +1,10 @@
 Config.Webhooks = { -- Place your Discord webhooks here.
-    ['createhouse'] = '',
-    ['deletehouse'] = '',
-    ['buyhouse'] = '',
+    ['createhouse'] = 'https://discord.com/api/webhooks/1144672406498181280/ME1SL5tpYMqM3ElQT0woYbJCC5TSsi_inzrxpzLBYk2ZToCzdz53c5M6myty1cq3Tl2G',
+    ['deletehouse'] = 'https://discord.com/api/webhooks/1144672519077507083/DUG0gYYJKkrGjJlSpOx9ZmvbFjVCrn9SJ_p1cc68HSWvQoHWq3FR9BjW9HW7uVN2bipV',
+    ['buyhouse'] = 'https://discord.com/api/webhooks/1144672331235590174/N5dAviGSuWPy1Olf5cyFyPKxEDX8sYkaWo1jrv43_ohJeBRA8U4XEy5puw5QkfccDG1m',
 }
 
-Config.WebhookMark = 'Quasar Housing Logs'
+Config.WebhookMark = 'Broski Housing Logs'
 
 Config.WebhookTranslates = { --Configure here all the translations of your webhook.
     ['createhouse'] = {
