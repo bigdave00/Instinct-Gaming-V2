5dd4ffd784a40632ff43493061143588a166901b
instinctgamingv2 <145374968+instinctgamingv2@users.noreply.github.com>
2023-09-28 17:06:03 +0100

    New Postal Map

--- resources/[standalone]/nearest-postal/fxmanifest.lua
+++ resources/[standalone]/nearest-postal/fxmanifest.lua
@@ -1,6 +1,6 @@
 -- the postal map to read from
 -- change it to whatever model you want that is in this directory
-local postalFile = 'ocrp-postals.json'
+local postalFile = 'new-postals.json'
 
 --[[
 WHAT EVER YOU DO, DON'T TOUCH ANYTHING BELOW UNLESS YOU **KNOW** WHAT YOU ARE DOING
